a80dc4ea92ce4355d5e2d79cde28ad005f1fd867
SweatyYeti <supersweatyyeti@gmail.com>
2024-03-08 23:14:10 -0800

    indentline context highlighting

--- lua/custom/plugins/indentline.lua
+++ lua/custom/plugins/indentline.lua
@@ -1,8 +1,22 @@
+vim.g.indent_blankline_context_patterns = { 'class', 'function', 'method', 'if', 'while', 'for' }
 return {
+
   -- Add indentation guides even on blank lines
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = {
+        enabled = true,
+        show_exact_scope = true,
+      },
+    },
+    -- config = function ()
+    --    require('indent-blankline').setup {
+    --
+    --
+    --
+    --    }
+    --  end,
   },
 }
