3304c2c18fb0447e25c744e03811f5144b6de566
SweatyYeti <supersweatyyeti@gmail.com>
2024-03-08 22:00:45 -0800

    format on keypress instead of save (for real this time)

--- lua/custom/plugins/autoformat.lua
+++ lua/custom/plugins/autoformat.lua
@@ -1,26 +1,35 @@
 return {
 
-    vim.keymap.set('n', '<leader>ff', ':lua vim.lsp.buf.format()\n', {}),
-    -- Uncomment to format on save
-    -- { -- Autoformat
-    --   'stevearc/conform.nvim',
-    --   opts = {
-    --     notify_on_error = false,
-    --     -- format_on_save = {
-    --     --   timeout_ms = 500,
-    --     --   lsp_fallback = true,
-    --     -- },
-    --     vim.keymap.set('n', '<leader>ff', ':lua vim.lsp.buf.format()\n', {}),
-    --     formatters_by_ft = {
-    --       lua = { 'stylua' },
-    --       -- Conform can also run multiple formatters sequentially
-    --       -- python = { "isort", "black" },
-    --       --
-    --       -- You can use a sub-list to tell conform to run *until* a formatter
-    --       -- is found.
-    --       -- javascript = { { "prettierd", "prettier" } },
-    --       --
-    --     },
-    --   },
-    -- },
+  -- Uncomment to format on save
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      -- format_on_save = {
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },
+        --
+      },
+    },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        '<leader>ff',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = 'n',
+        desc = 'Format buffer',
+      },
+    },
+  },
 }
