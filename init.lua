52130ca2a4d17fcbf88604fab69cf070dabd6c10
SweatyYeti <supersweatyyeti@gmail.com>
2024-03-10 20:36:36 -0700

    remove unecessary parentheses

--- init.lua
+++ init.lua
@@ -221,7 +221,7 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+require('lazy').setup{
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -673,7 +673,7 @@ require('lazy').setup({
       lazy = '💤 ',
     },
   },
-})
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
