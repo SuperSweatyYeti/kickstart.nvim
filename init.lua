a498a426a710711a0b6764fb21ff9b6e96353f7f
SweatyYeti <supersweatyyeti@gmail.com>
2024-03-11 21:45:51 -0700

    format

--- init.lua
+++ init.lua
@@ -654,7 +654,7 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
   { import = 'custom.themes' },
-}, { 
+}, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons otherwise define a unicode icons table
