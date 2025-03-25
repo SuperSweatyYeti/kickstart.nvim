f1ac2f29a4347b58a8c5aa624c7f09a84efeacb7
duster <supersweatyyeti@gmail.com>
2025-03-24 20:58:25 -0700

    toggle term descripton for which key

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return {
     opts = {
       --[[ things you want to change go here]]
       vim.keymap.set('t', 'ii', [[<C-\><C-n>]]),
-      vim.keymap.set('n', '<leader>t', [[:ToggleTerm<enter>]]),
+      vim.keymap.set('n', '<leader>t', [[:ToggleTerm<enter>]], {desc = 'ToggleTerm'}),
       vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], {}),
       vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], {}),
     },
