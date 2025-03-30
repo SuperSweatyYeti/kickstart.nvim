4d5edfe09ea09b39ee22d584324f6703120571d5
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-03-29 20:09:23 -0700

    will try out codecompanion later

---
+++ lua/custom/plugins/codecompanion.lua
@@ -0,0 +1,12 @@
+return {
+  -- Need to see docs for full setup at
+  -- https://codecompanion.olimorris.dev/
+  "olimorris/codecompanion.nvim",
+  enabled = false,
+  config = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+
+}
