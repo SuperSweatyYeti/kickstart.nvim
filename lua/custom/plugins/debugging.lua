8919123f0b14d0e19a5dac845c448f60d5eddf13
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-03-31 20:23:47 -0700

    debug update

--- lua/custom/plugins/debugging.lua
+++ lua/custom/plugins/debugging.lua
@@ -222,7 +222,7 @@ return {
     -- modified.
     dependencies = {
       {
-      'rcarriga/nvim-dap-ui',
+        'rcarriga/nvim-dap-ui',
         keys = {
           {
             '<leader>du',
@@ -233,7 +233,32 @@ return {
           },
         },
       },
+
       'nvim-neotest/nvim-nio',
+
+      {
+        'jay-babu/mason-nvim-dap.nvim',
+        ---@type MasonNvimDapSettings
+        opts = {
+          -- This line is essential to making automatic installation work
+          -- :exploding-brain
+          handlers = {},
+          automatic_installation = {
+            -- These will be configured by separate plugins.
+            exclude = {
+              -- 'delve',
+              -- 'python',
+            },
+          },
+          -- DAP servers: Mason will be invoked to install these if necessary.
+          ensure_installed = {
+            'bash',
+            'codelldb',
+            'php',
+            'debugpy',
+          },
+        },
+      },
       -- Specific languages here
       {
         'mfussenegger/nvim-dap-python',
