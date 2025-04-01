fc7cbc41fb791f33958cc6d9fc672a23b685988d
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-03-31 20:44:15 -0700

    mason updates

--- init.lua
+++ init.lua
@@ -469,6 +469,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
+        'debugpy',
+        'python-lsp-server',
+        'pylint',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
