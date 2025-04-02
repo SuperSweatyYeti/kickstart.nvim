48eba613c3485c04af6969f80e9b7e0f769b14e0
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-04-01 20:42:30 -0700

    udpate debugging remove powershell

--- lua/custom/plugins/debugging.lua
+++ lua/custom/plugins/debugging.lua
@@ -37,24 +37,6 @@ return {
         end,
       },
       {
-        -- Not working in linux
-        'Willem-J-an/nvim-dap-powershell',
-        enabled = false,
-        dependencies = {
-          'nvim-lua/plenary.nvim',
-          'mfussenegger/nvim-dap',
-          'rcarriga/nvim-dap-ui',
-          {
-            'm00qek/baleia.nvim',
-            lazy = true,
-            tag = 'v1.4.0',
-          },
-        },
-        config = function()
-          require('dap-powershell').setup()
-        end,
-      },
-      {
         'theHamsta/nvim-dap-virtual-text',
         -- config = true,
         dependencies = {
