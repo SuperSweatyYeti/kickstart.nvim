06eadc85bd8172f83e27bc502a1f54310596ae82
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-04-07 19:37:01 -0700

    add notification for using macros when using noice plugin

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -14,6 +14,34 @@ return {
   },
   config = function()
     require('noice').setup {
+
+      -- Recording macro notification
+      vim.api.nvim_create_autocmd('RecordingEnter', {
+        callback = function()
+          local msg = string.format('Register:  %s', vim.fn.reg_recording())
+          _MACRO_RECORDING_STATUS = true
+          vim.notify(msg, vim.log.levels.INFO, {
+            title = 'Macro Recording',
+            keep = function()
+              return _MACRO_RECORDING_STATUS
+            end,
+          })
+        end,
+        group = vim.api.nvim_create_augroup('NoiceMacroNotfication', { clear = true }),
+      }),
+
+      -- Recording macro notification
+      vim.api.nvim_create_autocmd('RecordingLeave', {
+        callback = function()
+          _MACRO_RECORDING_STATUS = false
+          vim.notify('Success!', vim.log.levels.INFO, {
+            title = 'Macro Recording End',
+            timeout = 2000,
+          })
+        end,
+        group = vim.api.nvim_create_augroup('NoiceMacroNotficationDismiss', { clear = true }),
+      }),
+
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
