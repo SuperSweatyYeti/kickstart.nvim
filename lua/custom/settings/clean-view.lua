14c931ce1faae25ea0898832694521c26475c330
SuperSweatyYeti <supersweatyyeti@gmail.com>
2025-05-02 10:59:37 -0700

    update clean-view toggle to be more compatible

--- lua/custom/settings/clean-view.lua
+++ lua/custom/settings/clean-view.lua
@@ -1,60 +1,83 @@
--- Toggle to get rid of all the virtualtext so that the buffer can
--- be more coducive to copying 
+-- Toggle a clean buffer view to make copying text easier
 local clean_view_enabled = false
 local saved_listchars = vim.opt.listchars:get()
+
 function CleanViewToggle()
   local gs_ok, gitsigns = pcall(require, "gitsigns")
-  local scope_ok, indentscope = pcall(require, "mini.indentscope")
-  local ns_id = vim.api.nvim_get_namespaces()["MiniIndentscope"]
+  local scope_ok, _ = pcall(require, "mini.indentscope")
 
-  if not clean_view_enabled then
+  if clean_view_enabled == false then
+    clean_view_enabled = true
+
+    -- UI: numbers, gutter, listchars
     vim.opt.number = false
     vim.opt.relativenumber = false
     vim.opt.signcolumn = "no"
     vim.opt.list = false
-    vim.wo.list = false -- window-local setting
+    vim.wo.list = false
+
+    -- Save/clear listchars
+    saved_listchars = vim.opt.listchars:get()
+
+    -- Disable virtual text plugins
     vim.cmd("IBLDisable")
     vim.cmd("TSContextDisable")
-    vim.diagnostic.disable()
-    -- Disable mini.indentscope
-    vim.b.miniindentscope_disable = true
-    if ns_id then
-      vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
+
+    -- Safely disable diagnostics if available
+    if vim.diagnostic and vim.diagnostic.disable then
+      vim.diagnostic.disable()
     end
-    -- Clear all virtual text (any remaining indent guides, decorators, etc.)
+
+    -- Disable mini.indentscope properly
+    vim.b.miniindentscope_disable = true
+    vim.cmd("doautocmd BufEnter")
+
+    -- Clear all virtual text namespaces
     for _, ns in pairs(vim.api.nvim_get_namespaces()) do
       vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
     end
-    -- Disable gitsigns
-    if gs_ok then
+
+    -- Detach gitsigns
+    if gs_ok and type(gitsigns.detach) == "function" then
       gitsigns.detach()
     end
 
     vim.cmd("silent! redraw")
-    clean_view_enabled = true
   else
+    clean_view_enabled = false
+
+    -- Re-enable UI
     vim.opt.number = true
     vim.opt.relativenumber = true
     vim.opt.signcolumn = "yes"
     vim.opt.list = true
     vim.wo.list = true
     vim.opt.listchars = saved_listchars
+
+    -- Re-enable plugins
     vim.cmd("IBLEnable")
     vim.cmd("TSContextEnable")
-    vim.diagnostic.enable()
+
+    -- Safely enable diagnostics if available
+    if vim.diagnostic and vim.diagnostic.enable then
+      vim.diagnostic.enable()
+    end
+
     -- Re-enable mini.indentscope
     vim.b.miniindentscope_disable = false
-    if scope_ok and type(indentscope.enable) == "function" then
-      indentscope.enable()
-    end
-    -- Re-enable gitsigns
+    vim.cmd("doautocmd BufEnter")
+
+    -- Reattach gitsigns
     if gs_ok and type(gitsigns.attach) == "function" then
       gitsigns.attach()
     end
-
-    clean_view_enabled = false
   end
 end
--- Add a keybind for it
-vim.api.nvim_set_keymap('n', '<leader>cv', '<cmd>lua CleanViewToggle()<enter>', { noremap = true, silent = true, desc = "[c]lean [v]iew [t]oggle Toggle" })
+
+-- Keymap
+vim.api.nvim_set_keymap('n', '<leader>cv', '<cmd>lua CleanViewToggle()<CR>', {
+  noremap = true,
+  silent = true,
+  desc = "[c]lean [v]iew Toggle"
+})
 
