6bb4a47eb31166476df1850eeb8d29c3fcad35bd
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-05-04 03:09:10 -0700

    paste buffer keymaps

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -4,22 +4,22 @@ return {
     -- Easier to change back to normal mode 'Double tap i'
     vim.keymap.set({ 'v', 'i' }, 'ii', '<Esc><Esc>', { desc = 'escape mode' }),
     -- Quickly get to end and beggining of line
-    vim.keymap.set({'n', 'v'}, '<S-h>', '<Home>', {}),
-    vim.keymap.set({'n', 'v'}, '<S-l>', '<End>', {}),
+    vim.keymap.set({ 'n', 'v' }, '<S-h>', '<Home>', {}),
+    vim.keymap.set({ 'n', 'v' }, '<S-l>', '<End>', {}),
     -- vim.keymap.set('v', 'ii', "<Esc>", {})
     -- Easier to navigate between panes
-    vim.keymap.set({'n','v'}, '<C-h>', '<C-w>h', {}),
-    vim.keymap.set({'n','v'}, '<C-j>', '<C-w>j', {}),
-    vim.keymap.set({'n','v'}, '<C-k>', '<C-w>k', {}),
-    vim.keymap.set({'n','v'}, '<C-l>', '<C-w>l', {}),
+    vim.keymap.set({ 'n', 'v' }, '<C-h>', '<C-w>h', {}),
+    vim.keymap.set({ 'n', 'v' }, '<C-j>', '<C-w>j', {}),
+    vim.keymap.set({ 'n', 'v' }, '<C-k>', '<C-w>k', {}),
+    vim.keymap.set({ 'n', 'v' }, '<C-l>', '<C-w>l', {}),
     -- Resize panes with hjkl
-    vim.keymap.set({'n','v'}, '<A-h>', '<C-w><', {}),
-    vim.keymap.set({'n','v'}, '<A-j>', '<C-w>+', {}),
-    vim.keymap.set({'n','v'}, '<A-k>', '<C-w>-', {}),
-    vim.keymap.set({'n','v'}, '<A-l>', '<C-w>>', {}),
+    vim.keymap.set({ 'n', 'v' }, '<A-h>', '<C-w><', {}),
+    vim.keymap.set({ 'n', 'v' }, '<A-j>', '<C-w>+', {}),
+    vim.keymap.set({ 'n', 'v' }, '<A-k>', '<C-w>-', {}),
+    vim.keymap.set({ 'n', 'v' }, '<A-l>', '<C-w>>', {}),
     -- Change tabs with hjkl
-    vim.keymap.set({'n','v'}, '<leader>Th', ':tabNext\n', {}),
-    vim.keymap.set({'n','v'}, '<leader>Tl', ':tabPrevious\n', {}),
+    vim.keymap.set({ 'n', 'v' }, '<leader>Th', ':tabNext\n', {}),
+    vim.keymap.set({ 'n', 'v' }, '<leader>Tl', ':tabPrevious\n', {}),
     -- vim.keymap.set('n', '<A-L>', ':tabNext\n', {}),
     -- vim.keymap.set('n', '<A-H>', ':tabPrevious\n', {}),
     --
@@ -29,6 +29,17 @@ return {
     vim.keymap.set('n', '<leader>Sv', ':vsplit\n', { desc = 'Split pane [v]ertically' }),
     -- Split Horizontal
     vim.keymap.set('n', '<leader>Sh', ':split\n', { desc = 'Split pane [h]orizontally' }),
+    -- Don't overwrite my paste buffer when pasting over text in visual mode
+    vim.keymap.set('v', 'p', function()
+      -- Paste now before swapping registers
+      vim.cmd 'normal! p'
+      local unnamed = vim.fn.getreginfo '"'
+      local zero = vim.fn.getreginfo '0'
+      -- Swap the contents
+      vim.fn.setreg('"', zero.regcontents, zero.regtype)
+      vim.fn.setreg('2', unnamed.regcontents, unnamed.regtype)
+    end, { desc = 'Swap last yank with previous yank' }),
+
     ------------------ END Custom Keymaps -----------------------------
   },
 }
