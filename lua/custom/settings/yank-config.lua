06b10cb2102bd43202c7f7cea5e5d84be8b645d3
Sweaty Yeti <supersweatyyeti@gmail.com>
2025-05-05 01:19:51 -0700

    update yank-config

--- lua/custom/settings/yank-config.lua
+++ lua/custom/settings/yank-config.lua
@@ -101,7 +101,7 @@ local destructive_motions = {
   { mode = 'n', key = 'dap' },
   { mode = 'n', key = 'dip' },
   -- visual mode
-  { mode = 'v', key = 'd' },
+  -- { mode = 'v', key = 'd' },
   { mode = 'v', key = 'c' },
   { mode = 'v', key = 'x' },
   { mode = 'v', key = 'p' },
