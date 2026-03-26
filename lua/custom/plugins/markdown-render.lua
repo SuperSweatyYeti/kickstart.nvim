3949b36c1f7851909899f952d1f6b9082d190b87
SuperSweatyYeti <supersweatyyeti@gmail.com>
2026-03-25 23:38:56 -0700

    markdown update

--- lua/custom/plugins/markdown-render.lua
+++ lua/custom/plugins/markdown-render.lua
@@ -3,6 +3,11 @@ return {
   opts = {
     file_types = { 'markdown', 'Avante' },
     language_map = {
+      -- NOTE: Add config to have markdown code block reconize code blocks for powershell
+      -- ```powershell
+      -- Get-Funciton
+      -- ```
+      --
       -- Config to add code block code recognition
       -- Here
       powershell = 'ps1',
@@ -25,6 +30,12 @@ return {
       -- Config to add code block code recognition
       -- Here
       powershell = 'ps1',
+    }
+
+    -- Keymaps
+    vim.keymap.set('n', '<leader>mr', '<cmd>RenderMarkdown toggle<CR>', { desc = '[m]arkdown [r]ender toggle', noremap = true, silent = true })
+    require('which-key').add {
+      { mode = { 'n' }, { '<leader>m', group = '[m]arkdown', hidden = false } },
     }
   end,
 }
