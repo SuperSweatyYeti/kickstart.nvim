917ce99080db597db2ddafadf3576cac0c4159bb
SuperSweatyYeti <supersweatyyeti@gmail.com>
2026-03-31 00:57:32 -0700

    update powershell config

--- lua/custom/plugins/powershell.lua
+++ lua/custom/plugins/powershell.lua
@@ -1,11 +1,25 @@
 return {
   {
-    -- NOTE: This plugin includes DAP config by default.
     'TheLeoP/powershell.nvim',
-    enabled = false,
+    enabled = true,
+    dependencies = {
+      'mfussenegger/nvim-dap',
+    },
     ---@type powershell.user_config
     opts = {
       bundle_path = vim.fn.stdpath 'data' .. '/mason/packages/powershell-editor-services',
     },
+    config = function(_, opts)
+      require('powershell').setup(opts)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'ps1',
+        callback = function(args)
+          -- vim.keymap.set({ 'n', 'x' }, '<leader>dpe', function() require('powershell').eval() end, { buffer = args.buf, desc = 'PowerShell: Eval' })
+          -- vim.keymap.set('n', '<leader>dpt', function() require('powershell').toggle_term() end, { buffer = args.buf, desc = 'PowerShell: Toggle Terminal' })
+          vim.keymap.set('n', '<leader>dpd', function() require('powershell').toggle_debug_term() end, { buffer = args.buf, desc = 'PowerShell: Toggle Debug Terminal' })
+        end,
+      })
+    end,
   },
 }
