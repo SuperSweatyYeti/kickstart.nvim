bb1028b479f8de5903a51a3474ae7a48fddd8b5c
Sweaty Yeti <supersweatyyeti@gmail.com>
2026-04-02 19:04:52 -0700

    update wrap mode

--- lua/custom/settings/wrap-line-movement.lua
+++ lua/custom/settings/wrap-line-movement.lua
@@ -7,15 +7,40 @@
 vim.g.wrap_mode_move = false
 vim.api.nvim_create_user_command('WrapModeMoveToggle', function()
   if vim.g.wrap_mode_move == false then
-    vim.keymap.set('n', 'j', 'gj', { noremap = true })
-    vim.keymap.set('n', 'k', 'gk', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'j', 'gj', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'k', 'gk', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '0', 'g0', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'H', 'g0', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '$', 'g$', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'L', 'g$', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '<Home>', 'g<Home>', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '<End>', 'g<End>', { noremap = true })
     vim.g.wrap_mode_move = true
   elseif vim.g.wrap_mode_move == true then
-    vim.keymap.set('n', 'j', 'j', { noremap = true })
-    vim.keymap.set('n', 'k', 'k', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'j', 'j', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'k', 'k', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '0', '0', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'H', '0', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '$', '$', { noremap = true })
+    vim.keymap.set({'n','v','x'}, 'L', '$', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '<Home>', '<Home>', { noremap = true })
+    vim.keymap.set({'n','v','x'}, '<End>', '<End>', { noremap = true })
     vim.g.wrap_mode_move = false
   end
 end, { desc = 'Toggle Up/Down movement to visual line instead of actual line' })
+
+-- Prepend wrap mode indicator to lualine section Y
+local lualine = require('lualine')
+local lualine_config = lualine.get_config()
+table.insert(lualine_config.sections.lualine_y, 1, {
+  function()
+    return '󰖶'
+  end,
+  cond = function()
+    return vim.g.wrap_mode_move == true
+  end,
+})
+lualine.setup(lualine_config)
 
 -- Add which key group
 require('which-key').add {
