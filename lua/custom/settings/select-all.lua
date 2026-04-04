fe676045b47d88164c7afcec2ab79b8e1180a973
SuperSweatyYeti <supersweatyyeti@gmail.com>
2026-04-04 02:12:16 -0700

    0.12 fix select-all yanks and deletes

--- lua/custom/settings/select-all.lua
+++ lua/custom/settings/select-all.lua
@@ -1,16 +1,23 @@
 -- Keymap to visually select everything in the current buffer
 vim.keymap.set('n', '<leader>vaa', 'ggVG', { noremap = true, desc = '[v]isually select [a]ll text from current buffer' })
--- Keymap to delete everything in the current buffer
-vim.keymap.set('n', '<leader>vad', 'ggVGd', { noremap = true, desc = '[v]isually select [a]ll text from current buffer and [d]elete' })
--- Keymap to yank everything in the current buffer
--- vim.keymap.set('n', '<leader>vay', function()
---   local win = 0
---   local pos = vim.api.nvim_win_get_cursor(win)
---   vim.cmd 'normal! ggVGy'
---   vim.api.nvim_win_set_cursor(win, pos)
--- end, { desc = '[v]isually select [a]ll text from current buffer and [y]ank' })
---
---
+-- Keymap to delete everything in the current buffer AND DON'T clobber system clipboard add to register 1 instead
+vim.keymap.set('n', '<leader>vad', function()
+  local prev_contents = vim.fn.getreg '"'
+  local prev_type = vim.fn.getregtype '"'
+  local prev_plus = vim.fn.getreg '+'
+  local prev_plus_type = vim.fn.getregtype '+'
+  local prev_star = vim.fn.getreg '*'
+  local prev_star_type = vim.fn.getregtype '*'
+
+  vim.cmd 'normal! ggVG"_d'
+
+  vim.fn.setreg('"', prev_contents, prev_type)
+  vim.fn.setreg('+', prev_plus, prev_plus_type)
+  vim.fn.setreg('*', prev_star, prev_star_type)
+end, { noremap = true, desc = '[v]isually select [a]ll text from current buffer and [d]elete' })
+
+
+
 -- Keymap to yank everything in the current buffer V2
 -- Add which-key-group
 require('which-key').add {
