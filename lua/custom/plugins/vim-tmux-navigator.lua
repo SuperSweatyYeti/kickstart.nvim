4d1b2c34eadc009627f998f968f876ac5c4f08bd
Sweaty Yeti <supersweatyyeti@gmail.com>
2026-08-04 16:09:15 -0700

    update tmuxnavigator plugin

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -19,26 +19,44 @@ return {
   --   },
   -- },
   -- NOTE: nvim herdr navigator
-  -- NOTE: nvim herdr navigator
   {
     'christoomey/vim-tmux-navigator',
     lazy = false,
+
+    init = function()
+      -- Prevent plugin from creating automatic mappings.
+      -- We will define them ourselves.
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+
     config = function()
-      local path = vim.fn.glob(vim.fn.expand('~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua'))
+      if is_os_windows() then
+        -- Windows: native Neovim pane navigation
+        vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+        vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+        vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+        vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
-      if path ~= '' then
-        -- Herdr exists, disable default tmux navigator mappings
-        vim.g.tmux_navigator_no_mappings = 1
-
-        dofile(path)
-      else
-        -- Herdr missing, restore normal vim-tmux-navigator mappings
-        vim.keymap.set('n', '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', { silent = true })
-        vim.keymap.set('n', '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>', { silent = true })
-        vim.keymap.set('n', '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>', { silent = true })
-        vim.keymap.set('n', '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>', { silent = true })
-        vim.keymap.set('n', '<C-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>', { silent = true })
+        return
       end
+
+      -- Non-Windows: try Herdr first
+      local herdr_path = vim.fn.glob(vim.fn.expand('~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua'))
+
+      if herdr_path ~= '' then
+        -- Herdr navigation
+        dofile(herdr_path)
+
+        return
+      end
+
+      -- Non-Windows + no Herdr:
+      -- Explicit vim-tmux-navigator mappings
+      vim.keymap.set('n', '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', { silent = true })
+      vim.keymap.set('n', '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>', { silent = true })
+      vim.keymap.set('n', '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>', { silent = true })
+      vim.keymap.set('n', '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>', { silent = true })
+      vim.keymap.set('n', '<C-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>', { silent = true })
     end,
   },
 }
