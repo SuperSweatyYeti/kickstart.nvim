96d877c3f7d58175e2fc3345b444809c603186ba
Sweaty Yeti <supersweatyyeti@gmail.com>
2026-08-04 16:05:03 -0700

    update tmux navigator keys

--- lua/custom/plugins/vim-tmux-navigator.lua
+++ lua/custom/plugins/vim-tmux-navigator.lua
@@ -19,16 +19,25 @@ return {
   --   },
   -- },
   -- NOTE: nvim herdr navigator
+  -- NOTE: nvim herdr navigator
   {
     'christoomey/vim-tmux-navigator',
     lazy = false,
-    init = function()
-      vim.g.tmux_navigator_no_mappings = 1
-    end,
     config = function()
       local path = vim.fn.glob(vim.fn.expand('~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua'))
+
       if path ~= '' then
+        -- Herdr exists, disable default tmux navigator mappings
+        vim.g.tmux_navigator_no_mappings = 1
+
         dofile(path)
+      else
+        -- Herdr missing, restore normal vim-tmux-navigator mappings
+        vim.keymap.set('n', '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>', { silent = true })
+        vim.keymap.set('n', '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>', { silent = true })
+        vim.keymap.set('n', '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>', { silent = true })
+        vim.keymap.set('n', '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>', { silent = true })
+        vim.keymap.set('n', '<C-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>', { silent = true })
       end
     end,
   },
